3b73bce89641c552806bb0f3dbdf1580e983a0ae
Honza Javorek <jan.javorek@gmail.com>
2013-01-24 22:43:23 +0100

    Fabric tasks for Heroku commands.

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 
 def deploy():
+    """Deploy site to Heroku."""
     # parse active branch
     branches = local('git branch --no-color 2> /dev/null', capture=True)
     match = re.search(r'\* ([\w\-_]*)', branches)
@@ -16,8 +17,24 @@ def deploy():
 
     # push to Heroku
     local('git push heroku {0}:master'.format(branch))
+    local('heroku ps:scale web=1')
 
     # push to Github
     tag = 'v' + datetime.utcnow().strftime('%Y.%m.%d')
     local('git tag {0}'.format(tag))
     local('git push --tags origin {0}:master'.format(branch))
+
+
+def ps():
+    """Show remote process list."""
+    local('heroku ps')
+
+
+def open():
+    """Open site in browser."""
+    local('heroku open')
+
+
+def logs():
+    """Show remote logs."""
+    local('heroku logs')
