81a3ff295257426805191a81a47ce1af1e4ca8f6
Honza Javorek <jan.javorek@gmail.com>
2013-01-24 23:36:05 +0100

    Markdown, templates, better versioning and deployment.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
-from datetime import datetime
 from setuptools import setup, find_packages
 
 
 setup(
     name='taborprekvapeni',
-    version=datetime.utcnow().strftime('%Y.%m.%d'),
+    version='0.0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'gunicorn==0.17.2',
         'gevent==0.13.8',
         'flask==0.9',
+        'flask-markdown==0.3',
     ],
 )
