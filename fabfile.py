187c43040a2048ef49dd6460331f244781607143
Honza Javorek <jan.javorek@gmail.com>
2013-03-09 15:52:48 +0100

    Fixed cache invalidation during deployment.

--- fabfile.py
+++ fabfile.py
@@ -82,12 +82,14 @@ def deploy():
     # redis to go
     if 'REDISTOGO_URL' not in capture('heroku config | grep REDISTOGO_URL'):
         local('heroku addons:add redistogo')
-    local('heroku run python flushcache.py')
 
     # push to Heroku
     local('git push heroku {0}:master'.format(branch))
     if 'web.1: up' not in capture('heroku ps'):
         local('heroku ps:scale web=1')
+
+    # wipe cache
+    local('heroku run python flushcache.py')
 
 
 def ps():
--- taborprekvapeni/__init__.py
+++ taborprekvapeni/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = '0.0.24'
+__version__ = '0.0.25'
 
 
 import logging
