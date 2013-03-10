93ffe1f67e85b16a217fde9744893bdbb0eb8272
Honza Javorek <jan.javorek@gmail.com>
2013-03-10 10:41:52 +0100

    Limiting Redis connections.

--- taborprekvapeni/__init__.py
+++ taborprekvapeni/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = '0.0.32'
+__version__ = '0.0.33'
 
 
 import logging
-from redis import StrictRedis as Redis
+from redis import StrictRedis as Redis, ConnectionPool as RedisPool
 
 from flask import Flask
 
@@ -14,7 +14,9 @@ app = Flask(__name__)
 app.config.from_object('taborprekvapeni.config')
 logging.basicConfig(**app.config['LOGGING'])
 
-redis = Redis.from_url(app.config['REDIS_URL'])
+redis_pool = RedisPool(max_connections=app.config['REDIS_MAX_CONNECTIONS'])
+redis = Redis.from_url(app.config['REDIS_URL'],
+                       connection_pool=redis_pool)
 
 
 from taborprekvapeni import views, templating
--- taborprekvapeni/config.py
+++ taborprekvapeni/config.py
@@ -16,6 +16,7 @@ LOGGING = {
 }
 
 REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+REDIS_MAX_CONNECTIONS = 10
 
 CACHE_EXPIRATION = 604800  # 1 week in seconds
 SEND_FILE_MAX_AGE_DEFAULT = 157680000  # 5 years in seconds
