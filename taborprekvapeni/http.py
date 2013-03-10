90f4028fd041af4d4607ebaf28e218ce3a0e5dae
Honza Javorek <jan.javorek@gmail.com>
2013-03-10 09:32:26 +0100

    User-Agent for requests.

--- taborprekvapeni/http.py
+++ taborprekvapeni/http.py
@@ -6,10 +6,14 @@ import requests
 from taborprekvapeni.cache import cache
 
 
+headers = {'User-Agent': ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:19.0) '
+                          'Gecko/20100101 Firefox/19.0')}
+
+
 def get(url):
     """Get contents from remote URL as bytes. Caches results."""
     def request():
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.content
     return cache(url, request)
