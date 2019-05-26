b119b1e924d4b227e58af7d33219be9ff9a0284c
Honza Javorek <mail@honzajavorek.cz>
2019-05-26 12:27:56 +0200

    fix what I've broken by markdown update

--- taborprekvapeni/__init__.py
+++ taborprekvapeni/__init__.py
@@ -10,7 +10,7 @@ from .cache import Cache, DevelopmentCache
 # config
 DEBUG = bool(os.getenv('TABORPREKVAPENI_DEBUG', False))
 LOGGING = {'format': '[%(levelname)s] %(message)s', 'level': logging.DEBUG}
-MARKDOWN = {'extensions': ['headerid'], 'output': 'html5'}
+MARKDOWN = {'extensions': ['toc'], 'output': 'html5'}
 CACHE_DEFAULT_TIMEOUT = 604800  # one week
 CACHE_DIR = path.realpath(path.join(path.dirname(__file__), '..', 'tmp'))
 GA_CODE = 'UA-1316071-16'
